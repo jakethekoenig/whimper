0d3223c48b6aef1b334ee4aa3439b17796825350
Jake Koenig <jakethekoenig@gmail.com>
2023-08-30 04:54:56 -0700

    Working checkpoint before plugin

--- whimper.py
+++ whimper.py
@@ -1,4 +1,6 @@
 import pyaudio
+import numpy as np
+import struct
 import pynvim
 import openai
 import os
@@ -7,47 +9,61 @@ from faster_whisper import WhisperModel
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-audio_file = "temp.wav"
-
+AUDIO_FILE = "temp.wav"
 CHUNK = 512
 RATE = 44100
 WHISPER_BATCH_SECS = 1
 GPT_BATCH_SECS = 4
-NUM_CHUNKS = 1000
+NUM_CHUNKS = 1200
 
 audio = pyaudio.PyAudio()
 # TODO: replace 3 with selection menu
 # I keep a buffer that's much larger than the chunks I read so I don't lose frames when I call whisper and or GPT.
 stream = audio.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True, input_device_index=3, frames_per_buffer=CHUNK*50)
 
-model_size = "tiny"
-model = WhisperModel(model_size)
+whisper_model_size = "small"
+whisper_model = WhisperModel(whisper_model_size)
 gpt_model = "gpt-3.5-turbo"
 system_prompt = {"role": "system",
                  "content": "The following is a transcription of spoken python. Write the transcript as syntactically valid python:"}
+
+def write_audio(data):
+    waveFile = wave.open(AUDIO_FILE, 'wb')
+    waveFile.setnchannels(1)
+    waveFile.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
+    waveFile.setframerate(RATE)
+    waveFile.writeframes(b''.join(data))	
+    waveFile.close()
+
+def segments_to_gpt_prompt(segments):
+    history = [system_prompt]
+    for segment in segments:
+        segment_transcript = {"role": "user", "content": "[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text)}
+        history.append(segment_transcript)
+    return history
+
 print("recording...")
-data = []
+data = [] 
+last_whisper_run = 0
+last_gpt_run = 0
+history = []
 for i in range(NUM_CHUNKS):
     data += [stream.read(CHUNK)]
-    if i % (RATE * WHISPER_BATCH_SECS // CHUNK) == 0:
-        waveFile = wave.open(audio_file, 'wb')
-        waveFile.setnchannels(1)
-        waveFile.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
-        waveFile.setframerate(44100)
-        waveFile.writeframes(b''.join(data))	
-        waveFile.close()
-        segments, info = model.transcribe(audio_file, beam_size=5)
-        history = [system_prompt]
-        for segment in segments:
-            segment_transcript = {"role": "user", "content": "[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text)}
-            history.append(segment_transcript)
-            print(segment_transcript)
-    # response = openai.ChatCompletion.create(
-        # model=gpt_model,
-        # messages=history
-        # # stream=True
-    # )
-    # print(response["choices"][0]["message"]["content"])
+    if (i - last_whisper_run) * CHUNK / RATE > WHISPER_BATCH_SECS:
+        last_whisper_run = i
+        # TODO: pass data directly to whisper
+        write_audio(data)
+        segments, info = whisper_model.transcribe(AUDIO_FILE, beam_size=5)
+        history = segments_to_gpt_prompt(segments)
+    if (i - last_gpt_run) * CHUNK / RATE > GPT_BATCH_SECS:
+        last_gpt_run = i
+        print(history)
+        response = openai.ChatCompletion.create(
+            model=gpt_model,
+            messages=history
+            # stream=True
+        )
+        print(response["choices"][0]["message"]["content"])
 
 print("finished recording")
 stream.close()
@@ -57,5 +73,8 @@ stream.close()
 class Whimper:
     def __init__(self, nvim):
         self.nvim = nvim
+
+    def listen_and_insert(self, args):
+
 
 
